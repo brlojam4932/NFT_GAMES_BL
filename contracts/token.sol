b3644fa7374e2b7ce9431e48ed62813080896939
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-07-21 16:19:13 -0700

    pet logic
    pet functions

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+pragma solidity ^0.8.0;
+
+// https://youtu.be/_VVqa7zWSxA
+// I BUILT NFT GAME IN 12 HOURS
+// with Fillip - Moralis Web3
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is ERC721, Ownable {
 
@@ -13,6 +18,8 @@ contract Token is ERC721, Ownable {
     uint256 endurance;
   }
 
+  uint256 nextId = 0;
+
   mapping(uint256 => Pet) private _tokenDetails;
 
   constructor(string memory name, string memory symbol) ERC721(name, symbol) {
@@ -20,8 +27,21 @@ contract Token is ERC721, Ownable {
   }
 
   function mint(uint8 damage, uint8 magic, uint8 endurance) public onlyOwner {
-
+    _tokenDetails[nextId] = Pet(damage, magic, block.timestamp, endurance);
+    _safeMint(msg.sender, nextId);
+    nextId++;
   }
+
+  function feed(uint256 tokeId) public {
+    Pet storage pet = _tokenDetails[nextId];
+    require(pet.endurance + pet.lastMeal > block.timestamp);
+    pet.lastMeal = block.timestamp;
+  }
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override {
+      Pet storage pet = _tokenDetails[nextId];
+      require(pet.endurance + pet.lastMeal > block.timestamp); // pet is still alive
+    }
 
 
 }
