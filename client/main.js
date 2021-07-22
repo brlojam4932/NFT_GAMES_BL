e7c5a427fbec126447c25322e1d1dd4aafe58eba
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-07-22 11:56:20 -0700

    finished NFT Game tut
    added html code and js code - got abi

--- client/main.js
+++ client/main.js
@@ -1,12 +1,13 @@
 
 Moralis.initialize("IgjTev92MjQUSMuHXIhc7A5KiFOGrtJ2RBgNTrz0"); // Application id from moralis.io
 Moralis.serverURL = "https://8a5ybyqvaz6q.usemoralis.com:2053/server"; //Server url from moralis.io
+const CONTRACT_ADDRESS = '0xdd118791ECD663883B912717c0661A86b568a334';
 
 async function init() {
     try {
         let user = Moralis.User.current();
-        if(!User){
-            $("#login_button").click( () => {
+        if(!user){
+            $("#login_button").click( async () => {
                 user = await Moralis.Web3.authenticate();
             })
         }
@@ -14,11 +15,39 @@ async function init() {
     } catch (error) {
         console.log(error);
     }
-    function renderGame() {
-        
-    }
+}
+
+async function renderGame() {
+    $("#login_button").hide();
+    //Get and render properties from smart contract
+    let petId = 0;
+    window.web3 = await Moralis.Web3.enable();
+    let abi = await getAbi();
+    let contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+    let data = await contract.methods.getTokenDetails(petId).call({from: ethereum.selectedAddress});
+    console.log(data);
+    renderPet(0, data);
+    $("#game").show();
+}
+
+function renderPet(id, data) {
+    $("#pet_id").html(id);
+    $("#pet_damage").html(data.damage);
+    $("#pet_magic").html(data.magic);
+    $("#pet_endurance").html(data.endurance);
+
+    let deathTime = new Date(parseInt(data.lastMeal) + parseInt(data.endurance) * 1000);
+
+    $("#pet_starvation_time").html(deathTime);
+}
+
+function getAbi(){
+    return new Promise((res) => {
+        $.getJSON("Token.json", ((json) => {
+            res(json.abi);
+        }))
+    })
+   
 }
 
 init();
-
-document.getElementById("login_button").onclick = login;
